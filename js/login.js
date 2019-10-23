796655b2862380d25849129167991d073d227dfd
matyas.levy <matyas.levy@juicymo.cz>
2019-10-23 20:50:09 +0200

    prvni commit

--- js/login.js
+++ js/login.js
@@ -4,6 +4,6 @@ const password = document.getElementById('password')
 const loginButton = document.getElementById('loginButton')
 loginButton.addEventListener("click",(e) => {
     e.preventDefault()
-    console.log(e);
     console.log(username.value)
+    
 })
