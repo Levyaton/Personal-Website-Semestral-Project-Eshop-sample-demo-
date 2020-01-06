a354cdd4e7b8464884a53e8c50a4a096edd73581
Levy, Matyáš <czech98@gmail.com>
2020-01-06 15:32:06 +0100

    Update db.php

--- models/db.php
+++ models/db.php
@@ -5,7 +5,7 @@
     $DATABASE_PASS = '';
     $DATABASE_NAME = '';
     
-    $link = htmlspecialchars($_SERVER['REQUEST_URI']);
+    $link = htmlspecialchars($_SERVER['SERVER_NAME']);
     if(strpos($link, "toad")){
         $DATABASE_HOST = '127.0.0.1';
         $DATABASE_USER = 'levymaty';
