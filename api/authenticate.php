b7673fe074df6a6786fbc4c70f43ec87eac5a227
matyas.levy <czech98@gmail.com>
2020-01-10 14:44:19 +0100

    commit

--- api/authenticate.php
+++ api/authenticate.php
@@ -36,12 +36,12 @@ if ($stmt->num_rows > 0) {
 		$_SESSION['name'] = htmlspecialchars($_POST["username"]);
 		$_SESSION['id'] = $id;
 		$_SESSION['theme'] = $theme;
-		//echo 'Welcome ' . $_SESSION['name'] . '!';
+		echo 'Welcome ' . $_SESSION['name'] . '!';
 	} else {
-		//echo 'Incorrect password!';
+		echo 'Incorrect password!';
 	}
 } else {
-	//echo 'Incorrect username!';
+	echo 'Incorrect username!';
 }
 $stmt->close();
 
@@ -69,14 +69,14 @@ if ($stmt->num_rows > 0) {
 		$_SESSION['name'] = htmlspecialchars($username);
 		$_SESSION['id'] = $id;
 		$_SESSION['theme'] = $theme;
-	//	echo 'Welcome ' . $_SESSION['name'] . '!';
+		echo 'Welcome ' . $_SESSION['name'] . '!';
 	} else {
-	//	echo 'Incorrect password!';
+		echo 'Incorrect password!';
 	}
 } else {
-//	echo 'Incorrect username!';
+	echo 'Incorrect username!';
 }
 $stmt->close();
 $link = htmlspecialchars($_POST["link"]);
-header("Location: ".$link."");
+//header("Location: ".$link."");
 ?>
